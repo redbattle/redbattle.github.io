1af1cc66b36067180c90bf7d8b4f06a29e8bbc60
ZhaiYu <zhaiyu_hb@hotmail.com>
2021-09-23 16:24:54 +0800

    feat: ci3

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@ set -e
 npm run build # 生成静态文件
 cd docs/.vuepress/dist # 进入生成的文件夹
 
-# if [ -z "$GITHUB_TOKEN" ]; then
-#   msg='deploy'
-#   githubUrl=git@github.com:redbattle/redbattle.github.io.git
-# else
-#   msg='来自github action的自动部署'
-#   githubUrl=https://redbattle:${GITHUB_TOKEN}@github.com/redbattle/redbattle.github.io.git
-#   git config --global user.name "redbattle"
-#   git config --global user.email "894072666@qq.com"
-# fi
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:redbattle/redbattle.github.io.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://redbattle:${GITHUB_TOKEN}@github.com/redbattle/redbattle.github.io.git
+  git config --global user.name "redbattle"
+  git config --global user.email "zhaiyu_hb@hotmail.com"
+fi
 
 git init
 git add .
-git commit -m 'deploy'
-git push -f git@github.com:redbattle/redbattle.github.io.git master:gh-pages # 推送到github
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 cd -
 rm -rf docs/.vuepress/dist
