079aea85996d2689136dfadf1ec11e5c5d1e9129
ZhaiYu <zhaiyu_hb@hotmail.com>
2021-09-23 09:41:32 +0800

    feat: ci

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
-
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
 git init
-git add -A
+git add -
 git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:redbattle/redbattle.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:redbattle/redbattle.github.io.git master:gh-pages # 推送到github
 
 cd -
+rm -rf docs/.vuepress/dist
