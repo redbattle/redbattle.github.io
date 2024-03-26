51e8aaae5be06853ffa1c3e068d73732ee472e21
redbattle <zhaiyu_hb@hotmail.com>
2024-03-26 14:43:37 +0800

    doc: prettier

--- components/EleContainer/src/index.vue
+++ components/EleContainer/src/index.vue
@@ -1,27 +1,17 @@
 <template>
   <el-container class="app-container">
     <el-aside class="relative" width="auto">
-      <div
-        class="text-gray-780 font-medium text-xl text-center app-container-logo"
-      >
-        管理平台
-      </div>
+      <div class="text-gray-780 font-medium text-xl text-center app-container-logo">管理平台</div>
       <el-menu
         class="app-container-menu"
         :class="{ 'app-container-menu-collapse': isCollapse }"
         :default-active="$route.meta.menuCode"
         :collapse="isCollapse"
       >
-        <el-menu-item-group
-          v-if="menuCode.includes($route.meta.menuCode)"
-          @click.native="clickMenu('user.list')"
-        >
+        <el-menu-item-group v-if="menuCode.includes($route.meta.menuCode)" @click.native="clickMenu('user.list')">
           <el-menu-item index="user">用户权限</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group
-          v-if="menuCode.includes($route.meta.menuCode)"
-          @click.native="clickMenu('home.index')"
-        >
+        <el-menu-item-group v-if="menuCode.includes($route.meta.menuCode)" @click.native="clickMenu('home.index')">
           <el-menu-item index="home">数据面板</el-menu-item>
         </el-menu-item-group>
       </el-menu>
@@ -34,11 +24,7 @@
           class="el-icon-s-unfold text-2xl mr-4 cursor-pointer"
           @click="handlerCollapse(false)"
         ></i>
-        <i
-          v-show="!isCollapse"
-          class="el-icon-s-fold text-2xl mr-4 cursor-pointen"
-          @click="handlerCollapse(true)"
-        ></i>
+        <i v-show="!isCollapse" class="el-icon-s-fold text-2xl mr-4 cursor-pointen" @click="handlerCollapse(true)"></i>
         <el-breadcrumb class="mr-auto" separator="/">
           <transition-group name="breadcrumb">
             <el-breadcrumb-item
@@ -54,11 +40,7 @@
           </transition-group>
         </el-breadcrumb>
         <div>
-          <el-dropdown
-            trigger="hover"
-            :hide-on-click="false"
-            @command="handleuser"
-          >
+          <el-dropdown trigger="hover" :hide-on-click="false" @command="handleuser">
             <el-button type="text">{{ userInfo.account }}</el-button>
             <el-dropdown-menu slot="dropdown">
               <el-dropdown-item command="logout">
@@ -68,10 +50,7 @@
           </el-dropdown>
         </div>
       </el-header>
-      <el-main
-        class="bg-gray-50 app-container-main"
-        :class="{ 'app-container-main-collapse': isCollapse }"
-      >
+      <el-main class="bg-gray-50 app-container-main" :class="{ 'app-container-main-collapse': isCollapse }">
         <transition name="fade-transform" mode="out-in">
           <slot />
         </transition>
@@ -85,58 +64,58 @@
  * 面包屑需要配合路由实现，见route.js
  */
 
-import { getCache, removeCache } from "cache.js";
-import { logout } from "api.js";
+import { getCache, removeCache } from 'cache.js'
+import { logout } from 'api.js'
 
 export default {
-  name: "Container",
+  name: 'Container',
   data() {
     return {
-      menuCode: "",
+      menuCode: '',
       userInfo: {},
-      isCollapse: false,
-    };
+      isCollapse: false
+    }
   },
   created() {
-    this.userInfo = getCache("m_userInfo");
-    this.menuCode = this.userInfo.permissionStr || "";
+    this.userInfo = getCache('m_userInfo')
+    this.menuCode = this.userInfo.permissionStr || ''
   },
   methods: {
     clickMenu(name) {
-      if (this.$route.name === name) return;
+      if (this.$route.name === name) return
       this.$router.push({
-        name,
-      });
+        name
+      })
     },
     handleuser(e) {
       switch (e) {
-        case "logout":
-          this.getLogout();
-          break;
+        case 'logout':
+          this.getLogout()
+          break
       }
     },
     getLogout() {
-      logout(); // 调退出接口
-      removeCache("m_userInfo"); // 移除用户信息
+      logout() // 调退出接口
+      removeCache('m_userInfo') // 移除用户信息
       // 跳转登录页面
       this.$router.replace({
-        path: "/login",
-      });
+        path: '/login'
+      })
     },
     handlerCollapse(collapse) {
-      this.isCollapse = collapse;
+      this.isCollapse = collapse
     },
     getQuery(item) {
-      const query = {};
+      const query = {}
       if (item?.query?.length) {
-        item.query.forEach((item) => {
-          query[item] = this.$route.query[item];
-        });
+        item.query.forEach(item => {
+          query[item] = this.$route.query[item]
+        })
       }
-      return query;
-    },
-  },
-};
+      return query
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
